5af0afe36c295b4842f319c4e66120cad197cf7f
axxdang <blue247114@gmail.com>
2025-11-29 05:12:50 +0700

    add android

---
+++ android/app/src/main/java/com/shipperTracker/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.shipperTracker.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.shipperTracker.app',
+  appName: 'shipperTracker',
+  webDir: 'dist'
+};
+
+export default config;
